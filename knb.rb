b89b7300d9cbceb222447c6dcb774a9a31e10823
ru <git@xoyo.ru>
2018-03-13 09:27:12 +0300

    knb

--- knb.rb
+++ knb.rb
@@ -1,21 +1,57 @@
-# Камень ножницы
-require 'tty/prompt'
-
-arr = ['камень','ножницы','бумага']
-score = 1
-
 while true
-	x = rand(0..2)
-	z = arr[x]
+    score = 0
+    puts '(k)амень (n)ожницы (b)умага'
+    print "Выбери букву (en) и нажми Enter: "
 
-	prompt = TTY::Prompt.new
-	answer = prompt.select("Выберите вариант", arr.shuffle)
-	if answer == z
-		score += 1
-		puts "Поздравляю! Ты выиграл! | Очков: #{score}"
-	else
-		score -= 1
-		puts "Ты проиграл! | Очков: #{score}"
-	end
+    player = gets.strip
+
+
+arr = [
+        
+        [:камень,:камень,:ничья],
+        [:бумага,:бумага,:ничья],
+        [:ножницы,:ножницы,:ничья],
+
+        [:камень,:бумага,:проигрыш],
+        [:камень,:ножницы,:победа],
+
+        [:бумага,:ножницы,:проигрыш],
+        [:бумага,:камень,:победа],
+
+        [:ножницы,:бумага,:победа],
+        [:ножницы,:камень,:проигрыш],        
+        
+        ]
+
+    comp = arr[rand(1..3)][0]
+
+    if player == "k"
+        player = :камень
+    elsif player == "n"
+        player = :ножницы
+    elsif player == "b"
+        player = :бумага
+    else
+        exit
+    end
+
+    if player == comp
+        puts "Ничья! Вы:#{player} Комп:#{comp}"
+    end
+
+
+
+    arr.each do |item|
+        if item[0] == player && item[1] == comp
+            if item[2] == :победа
+                puts "Вы победили Вы:#{player} Комп:#{comp}"
+                score +=1
+            elsif item[2] == :проигрыш
+                puts "Вы проиграли Вы:#{player} Комп:#{comp}"
+                score -=1
+            end
+            puts
+        end
+    end
 
 end
